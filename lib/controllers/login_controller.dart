062ecdf3f825b5ab7891caef0450b831665074b4
khalisha kaylanasywa <kkaylanasywa>
2024-11-24 14:51:42 +0700

    update_profile

--- lib/controllers/login_controller.dart
+++ lib/controllers/login_controller.dart
@@ -1,14 +1,15 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:talentaku/constants/app_colors.dart';
-import 'package:talentaku/views/login/login.dart';
+import '../constants/app_colors.dart';
 import '../constants/app_sizes.dart';
 import '../models/login_models.dart';
 import '../models/profile_image_picker.dart';
 import '../models/text_pair.dart';
 import '../models/text_field.dart'; // Import CustomTextFieldModel
 import '../views/login/login_pick_image.dart';
+import '../views/login/login.dart';
 
 class LoginController extends GetxController {
   // Login model for state management
@@ -20,7 +21,7 @@ class LoginController extends GetxController {
 
   // Observable for image picked state
   var isImagePicked = false.obs;
-  var profileImage = ''.obs; // Store the picked image path
+  var profileImage = ''.obs;
 
   // Function to create a CustomTextPair model
   CustomTextPairModel getPair() {
@@ -36,15 +37,14 @@ class LoginController extends GetxController {
   // Function to create a CustomTextPair model
   CustomTextPairModel getCustomTextPair() {
     return CustomTextPairModel(
-      primaryText: "Narendra",
-      secondaryText: "Siswa KB",
+      primaryText: "Selamat Datang",
+      secondaryText: "Semangat buat hari ini ya...",
       primaryStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 24, color: Colors.black),
       secondaryStyle: TextStyle(fontSize: 16, color: Colors.black),
       alignment: CrossAxisAlignment.start,
     );
   }
 
-  // Function to create CustomTextFieldModel for username
   CustomTextFieldModel getUsernameModel() {
     return CustomTextFieldModel(
       controller: usernameController,
@@ -53,7 +53,6 @@ class LoginController extends GetxController {
     );
   }
 
-  // Function to create CustomTextFieldModel for password
   CustomTextFieldModel getPasswordModel() {
     return CustomTextFieldModel(
       controller: passwordController,
@@ -63,22 +62,21 @@ class LoginController extends GetxController {
     );
   }
 
-  // Profile image picker model
   ProfileImagePickerModel getProfileImagePickerModel(BuildContext context) {
     return ProfileImagePickerModel(
       image: isImagePicked.value
-          ? AssetImage(profileImage.value) // Use the selected image
-          : AssetImage('assets/default_avatar.png'), // Use default avatar if no image selected
+          ? FileImage(File(profileImage.value)) // Use FileImage for local files
+          : AssetImage('') as ImageProvider, // Cast to ImageProvider
       avatarRadius: 60,
       cameraRadius: 20,
-      cameraBackgroundColor: Colors.blue,
+      cameraBackgroundColor: AppColors.backgroundLogin,
       cameraIcon: Icon(Icons.add_a_photo, color: AppColors.textLight),
       onCameraTap: () {
         showModalBottomSheet(
           context: context,
           builder: (context) {
             return Container(
-              padding: const EdgeInsets.all(16),
+              padding: const EdgeInsets.all(AppSizes.spaceM),
               height: 230,
               child: Column(
                 children: [
@@ -95,7 +93,7 @@ class LoginController extends GetxController {
                         label: "Camera",
                         onTap: () {
                           Navigator.pop(context);
-                          pickImageFromCamera(context);  // Open camera when Camera is selected
+                          pickImageFromCamera(context);
                         },
                       ),
                       buildOption(
@@ -103,7 +101,7 @@ class LoginController extends GetxController {
                         label: "Gallery",
                         onTap: () {
                           Navigator.pop(context);
-                          pickImageFromGallery(context); // Open gallery when Gallery is selected
+                          pickImageFromGallery(context);
                         },
                       ),
                     ],
@@ -117,44 +115,26 @@ class LoginController extends GetxController {
     );
   }
 
-  // Function to pick image from Camera
   Future<void> pickImageFromCamera(BuildContext context) async {
     final picker = ImagePicker();
-
-    // Pick image from the camera
     final XFile? pickedFile = await picker.pickImage(source: ImageSource.camera);
 
     if (pickedFile != null) {
-      // If an image is picked, you can handle the image file here
       profileImage.value = pickedFile.path;
       isImagePicked.value = true;
-      print("Image picked: ${pickedFile.path}");
-      // Optionally, you can also show the image preview in the UI here
-    } else {
-      // Handle case when user cancels camera action
-      print("No image selected.");
     }
   }
 
-  // Function to pick image from Gallery
   Future<void> pickImageFromGallery(BuildContext context) async {
     final picker = ImagePicker();
-
-    // Pick image from gallery
     final XFile? pickedFile = await picker.pickImage(source: ImageSource.gallery);
 
     if (pickedFile != null) {
-      // If an image is picked, update the state with the selected image
       profileImage.value = pickedFile.path;
       isImagePicked.value = true;
-      print("Image picked from gallery: ${pickedFile.path}");
-    } else {
-      // Handle case when user cancels gallery action
-      print("No image selected.");
     }
   }
 
-  // Option for selecting from camera or gallery
   static Widget buildOption({
     required IconData icon,
     required String label,
@@ -175,11 +155,10 @@ class LoginController extends GetxController {
   @override
   void onInit() {
     super.onInit();
-    usernameController.addListener(() => updateCredentials());
-    passwordController.addListener(() => updateCredentials());
+    usernameController.addListener(updateCredentials);
+    passwordController.addListener(updateCredentials);
   }
 
-  // Update login model with credentials
   void updateCredentials() {
     loginModel.value.updateLoginState(
       usernameController.text,
@@ -187,19 +166,20 @@ class LoginController extends GetxController {
     );
   }
 
-  // Validate form login
+  // Update the isValid function to check if username and password are at least 8 characters
   bool isValid() {
-    return loginModel.value.isValid;
+    return usernameController.text.length >= 8 &&
+        passwordController.text.length >= 8 &&
+        loginModel.value.isValid;
   }
 
-  // Handle login action
   void onLoginPressed(BuildContext context) {
     if (isValid()) {
       Get.to(() => LoginPickImage());
     } else {
       Get.snackbar(
         'Error',
-        'Username dan password harus diisi!',
+        'Username dan password harus diisi dan minimal 8 karakter!',
         backgroundColor: Colors.red,
         colorText: Colors.white,
         snackPosition: SnackPosition.TOP,
@@ -208,17 +188,14 @@ class LoginController extends GetxController {
     }
   }
 
-  // Reset the form fields
   void resetForm() {
     usernameController.clear();
     passwordController.clear();
-    isImagePicked.value = false;  // Reset the image picker state
-    profileImage.value = '';  // Clear the image path
+    isImagePicked.value = false;
+    profileImage.value = '';
   }
 
-  // Handle logout action
   void onLogoutPressed(BuildContext context) {
-    // Show logout confirmation snack bar
     Get.snackbar(
       'Anda Berhasil Logout',
       'Anda telah keluar dari akun Anda.',
